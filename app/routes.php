514e0d9e7cee4411a84aac69907f4e23c031602d
jrsalunga <jefferson.salunga@yahoo.com>
2014-09-25 13:27:22 +0800

    get env variables

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,28 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
-Route::get('/', function()
-{
+
+Route::get('/', function(){
 	return View::make('hello');
 });
+
+
+
+Route::get('/phpinfoko', function(){
+	return phpinfo();
+});
+
+Route::get('/env', function(){
+    return App::environment();
+});
+
+Route::get('/env/hostname', function(){
+    return gethostname();
+});
+
+Route::get('/checkdbconn', function(){
+	if(DB::connection()->getDatabaseName()){
+	   echo "connected sucessfully to database ".DB::connection()->getDatabaseName();
+	}
+});
+
